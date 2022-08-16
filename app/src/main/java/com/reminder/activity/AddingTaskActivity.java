c1233df4dd7e6bd6ca0545d5fe3c2ec280c85f99
Aamir Khan <aamirkhan8878@gmail.com>
2022-08-16 18:29:52 +0530

    Reminder App in Local DB

--- app/src/main/java/com/reminder/activity/AddingTaskActivity.java
+++ app/src/main/java/com/reminder/activity/AddingTaskActivity.java
@@ -1,15 +1,26 @@
 package com.reminder.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.reminder.R;
 import com.reminder.databinding.ActivityAddingTaskBinding;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import devs.mulham.horizontalcalendar.HorizontalCalendar;
+import devs.mulham.horizontalcalendar.HorizontalCalendarListener;
 
 
 public class AddingTaskActivity extends AppCompatActivity {
@@ -17,6 +28,8 @@ public class AddingTaskActivity extends AppCompatActivity {
     String[] convention;
     String selectedconvention;
     int sp_convention;
+    private HorizontalCalendar horizontalCalendar;
+    CalendarView simpleCalendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,7 +73,19 @@ public class AddingTaskActivity extends AppCompatActivity {
             }
         });
 
+        simpleCalendarView = (CalendarView) findViewById(R.id.calendar); // get the reference of CalendarView
+        simpleCalendarView.setFocusedMonthDateColor(Color.RED); // set the red color for the dates of  focused month
+        simpleCalendarView.setUnfocusedMonthDateColor(Color.BLUE); // set the yellow color for the dates of an unfocused month
+        simpleCalendarView.setSelectedWeekBackgroundColor(Color.RED); // red color for the selected week's background
+        simpleCalendarView.setWeekSeparatorLineColor(Color.GREEN); // green color for the week separator line
 
-
+        // perform setOnDateChangeListener event on CalendarView
+        simpleCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                // display the selected date by using a toast
+                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/reminder/activity/LoginActivity.java
+++ app/src/main/java/com/reminder/activity/LoginActivity.java
@@ -5,45 +5,84 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.reminder.MainActivity;
 import com.reminder.databinding.ActivityLoginBinding;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 
 public class LoginActivity extends AppCompatActivity {
-ActivityLoginBinding binding;
+    ActivityLoginBinding binding;
+    String stEmail = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= ActivityLoginBinding.inflate(getLayoutInflater());
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         String text = "Newbie?<font color=#4F597E> Create Account</font>";
         binding.txSignUp.setText(Html.fromHtml(text), TextView.BufferType.SPANNABLE);
 
+
         binding.txSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-            startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
             }
         });
         binding.txForgot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                startActivity(new Intent(LoginActivity.this,ForgotPasswordActivity.class));
+                startActivity(new Intent(LoginActivity.this, ForgotPasswordActivity.class));
             }
         });
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                stEmail = binding.etEmail.getText().toString().trim();
+                getMd5(stEmail);
 
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                Log.e("djfkdj", "onClick: "+ getMd5(stEmail));
+              startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         });
 
+
     }
+    public static String getMd5(String input) {
+        try {
+
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // digest() method is called to calculate message digest
+            //  of an input digest() return array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
